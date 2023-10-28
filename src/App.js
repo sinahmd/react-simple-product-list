45c6a6e30bcab6fcf9978601026760d861585f8e
sin <sinahamidi8@gmail.com>
2023-10-28 21:28:27 +0330

    add db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import ProductList from './component/ProsuctList/ProductList'
 import './App.css';
 import AddProduct from './component/AddProduct/AddProduct'
 
 const App = () =>{
-  const [products, setProducts] = useState([
-      { id:1, title:"book 1" },
-      { id:2, title:"book 2" },
-      { id:3, title:"book 3" },
-  ])
+  const [products, setProducts] = useState([])
+  useEffect(() =>{
+    const sendRequest = async () => {
+     const response = await fetch("http://localhost:7000/products")
 
-  const addProduct = (title) => {
-   const id = Math.floor(Math.random() * 10000)
-   const newProduct = {id, ...title }
+      const responseData = await response.json()
 
-   setProducts([...products, newProduct])
+      setProducts(responseData)
+
+    }
+    sendRequest()
+  },[])
+
+
+  const addProduct = async (title) => {
+   const response = await fetch("http://localhost:7000/products",{
+    method: 'POST',
+    Headers:{
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(title)
+   })
+    const responseData = await response.json
+
+   setProducts([...products, responseData])
   }
+  
 
-  const deleteProduct = (id) =>{
+  const deleteProduct = async (id) =>{
+   await fetch(`http://localhost:7000/products/${id}`,{
+      method: 'DELETE',
+    })
    setProducts (products.filter((item)=> item.id !== id))
   }
  
